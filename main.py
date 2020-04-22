b223ca7fcf6ba68ef581753e2c5455f3b1a8d302
Tarjei <tarjeibaerland@gmail.com>
2020-04-22 11:27:50 +0000

    Leksjon 3 lastet opp

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-xkoor = -150
-ykoor = 0
-bredde = 134
-hoyde = 231
+import colorsys
 
-svgstreng = f"""<svg viewBox="-200 -200 400 400" width="500mm" xmlns="http://www.w3.org/2000/svg">
- <rect x="-200" y="-200" width="400" height="400" fill="white" />
- <rect x="{xkoor}" y="{ykoor}" width="{bredde}" height="{hoyde}" stroke="black" fill="#ff4444" />
+xkoor1, ykoor1 = 10, 10
+xkoor2, ykoor2 = 55, 10
+xkoor3, ykoor3 = 10, 55
+xkoor4, ykoor4 = 55, 55
+
+bredde = 35
+hoyde = 35
+
+mainfill = "#ff4444"
+
+fill1 = mainfill
+fill2 = "#ffff44"
+fill3 = "#44ffff"
+fill4 = "#4444ff"
+
+
+
+
+svgstreng = f"""<svg viewBox="0 0 100 100" width="500mm" height="500mm" xmlns="http://www.w3.org/2000/svg">
+ <rect x="0" y="0" width="100%" height="100%" fill="white" />
+ <rect x="{xkoor1}" y="{ykoor1}" width="{bredde}" height="{hoyde}" stroke="black" fill="{fill1}" />
+ <rect x="{xkoor2}" y="{ykoor2}" width="{bredde}" height="{hoyde}" stroke="black" fill="{fill2}" />
+ <rect x="{xkoor3}" y="{ykoor3}" width="{bredde}" height="{hoyde}" stroke="black" fill="{fill3}" />
+ <rect x="{xkoor4}" y="{ykoor4}" width="{bredde}" height="{hoyde}" stroke="black" fill="{fill4}" />
 </svg>"""
 
-filnavn = "leksjon2-v2.svg"
+filnavn = "leksjon3.svg"
 
 # Vi må fortelle Python at vi vil 'åpne' en fil
 fd = open(filnavn, "w")   # 'fd' er nå et filobjekt
